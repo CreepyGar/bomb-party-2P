ea14bfc26dc13fcdd343045cdc4755877834c1f2
CreepyGar <arthur.verstraeten@gmail.com>
2022-05-10 12:58:39 +0200

    Added colored output and finished implementations

--- 2P.py
+++ 2P.py
@@ -1,8 +1,7 @@
-#bomb party 2P: give a prompt and get a word from an implemented dictionary, request for a prompt loops infinitely unless the user terminates
 import random
-exit = False
+from colorama import Fore, Style
+
 word_list = []
-#initializing a differently shuffeled word list at the start of every game
 def initialize():
     with open('dict.txt') as f:
         for line in f:
@@ -10,59 +9,58 @@ def initialize():
     return None
 
 def game_loop(mode):
-    prompt = str(input("Give the current prompt:")).upper()
+    prompt = str(input("Give the current prompt: ")).upper()
     if prompt == "TERMINATE":
         return 0
-    
     if mode == "SHORT":
         solution = "how would you react if you saw a donkey eating your figs lmao"
         for word in word_list: 
             if (prompt in word and len(word) < len(solution)): solution = word
-        if solution == "how would you react if you saw a donkey eating your figs lmao": solution = "NO AVAILABLE WORDS LOL!"
+        if solution == "how would you react if you saw a donkey eating your figs lmao": print("NO AVAILABLE WORDS LOL!")
         else: 
-            number = str(word_list.index(word))
             word_list.remove(word)
-            print("SOLUTION: " + solution + "Index:" + number)
-    
+            print(Fore.GREEN + "SOLUTION: " + Fore.RED + solution + Style.RESET_ALL)
     elif mode == "LONG":
         solution = ""
         for word in word_list: 
             if (prompt in word and len(word) > len(solution)): solution = word
-        if solution == "": solution = "NO AVAILABLE WORDS LOL!"
-        else: word_list.remove(word)
-    
+        if solution == "": print("NO AVAILABLE WORDS LOL!" + "")
+        else: 
+            word_list.remove(word)
+            print(Fore.GREEN + "SOLUTION: " + Fore.RED + solution + Style.RESET_ALL)
     elif mode == "RAND" or mode == "ALFABET":
         solution = ""
         for word in word_list: 
             if prompt in word:
                 solution = word
                 break
-        if solution == "": solution = "NO AVAILABLE WORDS LOL!"
-        else: word_list.remove(word)
-    
+        if solution == "": print("NO AVAILABLE WORDS LOL!" + "")
+        else: 
+            number = str(word_list.index(word))
+            word_list.remove(word)
+            print(Fore.GREEN + "SOLUTION: " + Fore.RED + solution + Fore.GREEN + "Index:    " + Fore.RED + number + Style.RESET_ALL)
     elif mode == "BACK-ALFABET":
         solution = ""
-        for index in range(len(word_list - 1), -1, -1): 
+        for index in range(len(word_list) - 1, -1, -1): 
             if prompt in word_list[index]:
                 solution = word_list[index]
                 break
-        if solution == "": solution = "NO AVAILABLE WORDS LOL!"
-        else: word_list.remove(solution)
-    
-    
-    
-    
-    print("SOLUTION: " + solution)
+        if solution == "": print("NO AVAILABLE WORDS LOL!" + "")
+        else: 
+            number = str(len(word_list)-(word_list.index(solution) + 1))
+            word_list.remove(solution)
+            print(Fore.GREEN + "SOLUTION: " + Fore.RED + solution + Fore.GREEN + "Index:    " + Fore.RED + number + Style.RESET_ALL)
     return game_loop(mode)
 
 def options_menu():
     while True:
-        text = input("SELECT MODE: LONG/SHORT/RAND/ALFABET/BACK-ALFABET").upper()
+        text = input("SELECT MODE LONG/SHORT/RAND/ALFABET/BACK-ALFABET/EXIT: ").upper()
         initialize()
         if text == "RAND" or text == "LONG" or text == "SHORT":
             random.shuffle(word_list)
+        if text == "EXIT":
+            return
         game_loop(text)
         return options_menu()
 
 options_menu()
-lol = 1
